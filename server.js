1a6895a5021ea5b832367d81a651a79c9d617068
SamDz16 <samdz161999@gmail.com>
2019-11-19 14:10:57 +0100

    "new updates"

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
+//Requirements
 var express = require('express');
 var server = express();
+var bodyParser = require('body-parser');
 
-var controller = require('./manage/controller');
+//Uses
+server.use('/assets', express.static('public'));
+server.use(bodyParser.urlencoded({ extend: true }));
+server.use(bodyParser.json);
 
-server.use('/assets', express.static('./assets'));
-
+//View Engine - EJS
 server.set('view engine', 'ejs');
 
-controller(server);
+//Routers
+server.get('/', function(req, res) {
+	res.render('choixUtilisateur.ejs');
+});
 
+server.post('/', (req, res, next) => {
+	console.log(req.body);
+});
+
+//Listening to port
 server.listen(7000, () => {
 	console.log('The server is listening ...');
 });
