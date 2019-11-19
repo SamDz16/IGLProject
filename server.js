7e58beb39c0245c3b27942e25e42d3c4e90fd051
SamDz16 <samdz161999@gmail.com>
2019-11-19 16:24:15 +0100

    Manage the router '/' + Redirecting depending on the user mode

--- server.js
+++ server.js
@@ -4,22 +4,30 @@ var server = express();
 var bodyParser = require('body-parser');
 
 //Uses
-server.use('/assets', express.static('public'));
+server.use('/assets', express.static('./assets'));
 server.use(bodyParser.urlencoded({ extend: true }));
-server.use(bodyParser.json);
+//server.use(bodyParser.json);
 
 //View Engine - EJS
 server.set('view engine', 'ejs');
 
 //Routers
 server.get('/', function(req, res) {
-	res.render('choixUtilisateur.ejs');
+	res.render('choixUtilisateur');
 });
 
 server.post('/', (req, res, next) => {
-	console.log(req.body);
+	var choice = req.body;
+	if (choice.admin == 'on') {
+		//** Something to develop**//
+	} else {
+		res.redirect('student');
+	}
 });
 
+server.get('/student', function(req, res) {
+	res.render('Etudiant');
+});
 //Listening to port
 server.listen(7000, () => {
 	console.log('The server is listening ...');
