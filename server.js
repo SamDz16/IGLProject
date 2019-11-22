e75a3384e368dc495b64db6e17612fd4af637d9f
WissemAmr <46846226+WissemAmr@users.noreply.github.com>
2019-11-22 12:25:54 +0100

    insertion des demandes de permutations dans la bdd
    la base de donnée qu'on a utilisé MONGO

--- server.js
+++ server.js
@@ -4,17 +4,34 @@ var path = require('path');
 var bodyParser = require('body-parser');
 const MongoClient = require ('mongodb').MongoClient; 
 
-MongoClient.connect ('mongodb://localhost:27017', (err,client)=>{
-	console.log('connected to mongo'); 
-	let db=client.db('demande'); 
-	client.close();
-});
+
 server.get('/', function(req, res, next) {
 	res.sendFile(path.join(__dirname, 'interface', 'Etudiant.html'));
 });
 
 server.post('/Etudiant', bodyParser.urlencoded({ extend: true }), (req, res, next) => {
-	res.send(req.body);
+	
+	MongoClient.connect ('mongodb://localhost:27017/IGL', (err,client)=>{
+	console.log('connected to mongo'); 
+	const db=client.db(); 
+	db.collection ('demande').insertOne({  
+		nom :req.body.nom,
+		prenom :req.body.prenom ,
+		matricule :req.body.matricule,
+		email :req.body.email,  
+		groupeA :req.body.grrA, 
+		groupeV: req.body.grV
+
+	}).then (result=> { 
+		console.log ('votre demande a été enregistré');
+		res.redirect ('/');
+	}) 
+	
+
+	client.close();
 });
-server.listen(7000,'127.0.0.1');
+});
+
+
+server.listen(3001,'127.0.0.1');
 	console.log('The server is listening ...');
