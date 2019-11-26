44f6c7d8179af724487755d26d66b36153f67ade
SamDz16 <samdz161999@gmail.com>
2019-11-26 17:05:21 +0100

    Integration of work + Designing the admin page

--- server.js
+++ server.js
@@ -1,44 +1,32 @@
 var express = require('express');
 var server = express();
-var path = require('path');
 var bodyParser = require('body-parser');
 const MongoClient = require ('mongodb').MongoClient; 
 
+//View Engine - EJS
 server.set ('view engine','ejs'); 
 
+//Uses
+server.use('/assets', express.static('./assets'));
 
+//Routers
 server.get('/', function(req, res, next) {
-	res.render('Etudiant')});
+	res.render('choixUtilisateur')});
 
-	
+server.post('/', bodyParser.urlencoded({ extend: true }), (req, res) => {
+	var choice = req.body;
+	if (choice.admin == 'on') {
+		res.redirect('/admin');
+	} else {
+		res.redirect('/etudiant');
+	}
+});
 
+server.get('/etudiant', function(req, res) {
+	res.render('Etudiant');
+});
 
-
-server.post('/Consultation', function(req, res, next) {
-	MongoClient.connect ('mongodb://localhost:27017/IGL', (err,client)=>{
-		console.log('connected to mongo'); 
-		const db=client.db(); 
-		db.collection ('demande').find().toArray().then(demande=>{
-			console.log(demande)
-			res.render('Consultation',{ 
-				demande:demande // res.send (demande)
-			}) // quand il clique sur le button consulter
-		})
-		client.close();
-		}) 
-
-}); 
-
-	
-//server.post('/Consultation', bodyParser.urlencoded({ extend: true }), (req, res, next) => {
-	
-
-//})	
-
-
-
-
-server.post('/Etudiant', bodyParser.urlencoded({ extend: true }), (req, res, next) => {
+server.post('/etudiant', bodyParser.urlencoded({ extend: true }), (req, res, next) => {
 	
 	MongoClient.connect ('mongodb://localhost:27017/IGL', (err,client)=>{
 	console.log('connected to mongo'); 
@@ -58,20 +46,29 @@ server.post('/Etudiant', bodyParser.urlencoded({ extend: true }), (req, res, nex
 		res.redirect ('/');
 	}) 
 	
-
 	client.close();
-});
+	});
 }); 
 	
-// la partie admin 
- 
+server.post('/Consultation', function(req, res, next) {
+	MongoClient.connect ('mongodb://localhost:27017/IGL', (err,client)=>{
+		console.log('connected to mongo'); 
+		const db=client.db(); 
+		db.collection ('demande').find().toArray().then(demande=>{
+			console.log(demande)
+			res.render('Consultation',{ 
+				demande:demande // res.send (demande)
+			}) // quand il clique sur le button consulter
+		})
+		client.close();
+		}) 
+
+}); 
+
+//La partie admin 
 server.get('/admin', function(req, res, next) {
 	res.render('admin')});
 
-
-
-
-
-//la partie serveur
+//La partie serveur
 server.listen(3001,'127.0.0.1');
 	console.log('The server is listening ...');
