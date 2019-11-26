e8278f03a0ac036917b42f19533ce5ebb671362b
SamDz16 <samdz161999@gmail.com>
2019-11-26 18:52:10 +0100

    Add docker files

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var server = express();
 var bodyParser = require('body-parser');
-const MongoClient = require ('mongodb').MongoClient; 
+const MongoClient = require('mongodb').MongoClient;
 
 //View Engine - EJS
-server.set ('view engine','ejs'); 
+server.set('view engine', 'ejs');
 
 //Uses
 server.use('/assets', express.static('./assets'));
 
 //Routers
 server.get('/', function(req, res, next) {
-	res.render('choixUtilisateur')});
+	res.render('choixUtilisateur');
+});
 
 server.post('/', bodyParser.urlencoded({ extend: true }), (req, res) => {
 	var choice = req.body;
@@ -27,48 +28,53 @@ server.get('/etudiant', function(req, res) {
 });
 
 server.post('/etudiant', bodyParser.urlencoded({ extend: true }), (req, res, next) => {
-	
-	MongoClient.connect ('mongodb://localhost:27017/IGL', (err,client)=>{
-	console.log('connected to mongo'); 
-	const db=client.db(); 
-	db.collection ('demande').updateOne({ email :req.body.email},  
-		{$set:{
-		nom :req.body.nom,
-		prenom :req.body.prenom ,
-		matricule :+req.body.matricule, 
-		groupeA :+req.body.grA, 
-		groupeV: +req.body.grV
+	MongoClient.connect('mongodb://localhost:27017/IGL', (err, client) => {
+		console.log('connected to mongo');
+		const db = client.db();
+		db
+			.collection('demande')
+			.updateOne(
+				{ email: req.body.email },
+				{
+					$set: {
+						nom: req.body.nom,
+						prenom: req.body.prenom,
+						matricule: req.body.matricule,
+						groupeA: +req.body.grA,
+						groupeV: +req.body.grV
+					}
+				},
+				{ upsert: true }
+			)
+			.then((result) => {
+				console.log('votre demande a été enregistré');
 
-	}},
-	{upsert:true}).then (result=> { 
-		console.log ('votre demande a été enregistré');
-		
-		res.redirect ('/');
-	}) 
-	
-	client.close();
-	});
-}); 
-	
-server.post('/Consultation', function(req, res, next) {
-	MongoClient.connect ('mongodb://localhost:27017/IGL', (err,client)=>{
-		console.log('connected to mongo'); 
-		const db=client.db(); 
-		db.collection ('demande').find().toArray().then(demande=>{
-			console.log(demande)
-			res.render('Consultation',{ 
-				demande:demande // res.send (demande)
-			}) // quand il clique sur le button consulter
-		})
+				res.redirect('/');
+			});
+
 		client.close();
-		}) 
+	});
+});
 
-}); 
+server.post('/Consultation', function(req, res, next) {
+	MongoClient.connect('mongodb://localhost:27017/IGL', (err, client) => {
+		console.log('connected to mongo');
+		const db = client.db();
+		db.collection('demande').find().toArray().then((demande) => {
+			console.log(demande);
+			res.render('Consultation', {
+				demande: demande // res.send (demande)
+			}); // quand il clique sur le button consulter
+		});
+		client.close();
+	});
+});
 
-//La partie admin 
+//La partie admin
 server.get('/admin', function(req, res, next) {
-	res.render('admin')});
+	res.render('admin');
+});
 
 //La partie serveur
-server.listen(3001,'127.0.0.1');
-	console.log('The server is listening ...');
+server.listen(3001, '127.0.0.1');
+console.log('The server is listening ...');
