2b4dcd92e819952c11d9c8dbfec0c5a86154f02e
SamDz16 <samdz161999@gmail.com>
2019-12-25 21:50:58 +0100

    Adding the docker-compose.yml file for the momgoDb image + Modifing the server.js file

--- server.js
+++ server.js
@@ -14,16 +14,20 @@ server.get('/', function (req, res, next) {
 	res.render('choixUtilisateur');
 });
 
-server.post('/', bodyParser.urlencoded({
-	extend: true
-}), (req, res) => {
-	var choice = req.body;
-	if (choice.admin == 'on') {
-		res.redirect('/admin');
-	} else {
-		res.redirect('/etudiant');
+server.post(
+	'/',
+	bodyParser.urlencoded({
+		extend: true
+	}),
+	(req, res) => {
+		var choice = req.body;
+		if (choice.admin == 'on') {
+			res.redirect('/admin');
+		} else {
+			res.redirect('/etudiant');
+		}
 	}
-});
+);
 
 server.get('/etudiant', function (req, res) {
 	res.render('Etudiant');
@@ -33,7 +37,7 @@ server.post('/etudiant', bodyParser.urlencoded({
 	extend: true
 }), (req, res, next) => {
 	MongoClient.connect('mongodb://localhost:27017/IGL', (err, client) => {
-		console.log('connected to mongo');
+		//console.log('connected to mongo');
 		const db = client.db();
 		db
 			.collection('demande')
@@ -51,7 +55,7 @@ server.post('/etudiant', bodyParser.urlencoded({
 				upsert: true
 			})
 			.then((result) => {
-				console.log('votre demande a été enregistré');
+				console.log('Votre demande a été enregistrée');
 
 				res.redirect('/');
 			});
@@ -68,7 +72,7 @@ server.post('/Consultation', function (req, res, next) {
 			console.log(demande);
 			res.render('Consultation', {
 				demande: demande // res.send (demande)
-			}); // quand il clique sur le button consulter
+			}); // Quand il clique sur le button consulter
 		});
 		client.close();
 	});
