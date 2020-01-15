08bc5bba058664530171621c96b623cc758d2dd6
SamDz16 <samdz161999@gmail.com>
2020-01-15 19:11:12 +0100

    Selenium issue

--- seleniumTest/seleniumTest.js
+++ seleniumTest/seleniumTest.js
@@ -11,71 +11,71 @@ const webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
 
-describe("Testing adding a student to the database using SELENIUM", () => {
+// describe("Testing adding a student to the database using SELENIUM", () => {
 
-    beforeEach(function () {
-        driver.get('http://localhost:7000');
-        driver.manage().window().maximize();
-    })
+//     beforeEach(function () {
+//         driver.get('http://localhost:7000');
+//         driver.manage().window().maximize();
+//     })
 
-    afterEach(function () {
-        // driver.sleep(5000); //Sleep the driver for five (05) seconds
-        driver.quit();
-    })
+//     afterEach(function () {
+//         driver.sleep(2000); //Sleep the driver for five (05) seconds
+//         driver.quit();
+//     })
 
-    it('Ok, Adds new student to the database', function (done) {
+//     it('Ok, Adds new student to the database', function (done) {
 
-        var driver = new webdriver.Builder().forBrowser('chrome').build();
+var driver = new webdriver.Builder().forBrowser('chrome').build();
 
-        // driver.get('http://localhost:7000').then(function () {
-        // driver.manage().window().maximize();
-        driver.findElement(By.id('student-radio')).click().then(function () {
+// driver.get('http://localhost:7000').then(function () {
+// driver.manage().window().maximize();
+driver.findElement(By.id('student-radio')).click().then(function () {
 
-            //When the student radio button is selected, we click the button ...
-            driver.findElement(By.id('btn-suivant')).click().then(function () {
+    //When the student radio button is selected, we click the button ...
+    driver.findElement(By.id('btn-suivant')).click().then(function () {
 
-                //Retreive all the input in the form ...
-                let nom = driver.findElement(By.name('nom'));
-                let prenom = driver.findElement(By.name('prenom'));
-                let matricule = driver.findElement(By.name('matricule'));
-                let email = driver.findElement(By.name('email'));
-                let grA = driver.findElement(By.name('grA'));
-                let grV = driver.findElement(By.name('grV'));
+        //Retreive all the input in the form ...
+        let nom = driver.findElement(By.name('nom'));
+        let prenom = driver.findElement(By.name('prenom'));
+        let matricule = driver.findElement(By.name('matricule'));
+        let email = driver.findElement(By.name('email'));
+        let grA = driver.findElement(By.name('grA'));
+        let grV = driver.findElement(By.name('grV'));
 
-                //And then we fill in them all ...
-                nom.sendKeys('testNom');
-                prenom.sendKeys('testPrenom');
-                matricule.sendKeys('17/000');
-                email.sendKeys('test_selenium@esi.dz');
-                grA.sendKeys(1);
-                grV.sendKeys(9);
+        //And then we fill in them all ...
+        nom.sendKeys('testNom');
+        prenom.sendKeys('testPrenom');
+        matricule.sendKeys('17/000');
+        email.sendKeys('test_selenium@esi.dz');
+        grA.sendKeys(1);
+        grV.sendKeys(9);
 
-                driver.findElement(By.css('.button1')).click().then(function () {
+        driver.findElement(By.css('.button1')).click().then(function () {
 
-                    console.log('The student has been added to the database');
+            console.log('The student has been added to the database');
 
-                    mongoose.connect('mongodb://localhost:27017/PERMUTATION', {
-                        useUnifiedTopology: true,
-                        useNewUrlParser: true
-                    });
+            mongoose.connect('mongodb://localhost:27017/PERMUTATION', {
+                useUnifiedTopology: true,
+                useNewUrlParser: true
+            });
 
-                    //Testing the connection to the database
-                    mongoose.connection.once('open', () => {
+            //Testing the connection to the database
+            mongoose.connection.once('open', () => {
 
-                        console.log("Connected to the database");
-                        // it('should do something...', function (done) {
-                        Astudent.findOne({
-                            matricule: '17/000'
-                        }).then(function (result) {
-                            assert(result.nom === 'testNom');
-                            done();
-                        })
-
-                    }).on('error', (error) => {
-                        console.log('Error while connecting to the database : ', error);
-                    })
+                console.log("Connected to the database");
+                // it('should do something...', function (done) {
+                Astudent.findOne({
+                    matricule: '17/000'
+                }).then(function (result) {
+                    assert(result.nom === 'testNom');
+                    done();
                 })
+
+            }).on('error', (error) => {
+                console.log('Error while connecting to the database : ', error);
             })
         })
     })
 })
+//     })
+// })
---
+++ testSelenium.js
@@ -0,0 +1,19 @@
+var test = require('selenium-webdriver/testing');
+var assert = require('assert');
+// var webdriver = require('selenium-webdriver'),
+//     {
+//         describe,
+//         it,
+//         before,
+//         after
+//     } = require('selenium-webdriver'),
+//     By = webdriver.By,
+//     until = webdriver.until;
+
+test.describe('ghjbknkjnk', function () {
+    test.it('hgjhkj', function (done) {
+
+        expect(5).equals(3 + 2);
+        done();
+    })
+})
